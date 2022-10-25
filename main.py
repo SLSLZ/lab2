628b6819adfab6f72e751673720c0fa255f2690d
SeanLim <seanlim.21@ichat.sp.edu.sg>
2022-10-25 10:38:49 +0800

    second commit with BMI? actl third but might have some error before

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print  ("Test first print")
+
+def calculate_bmi(height, weight):
+    print("Height = " + str(height))
+    print("Weight = " + str(weight))
+
+    bmi = (weight)/(height*height)
+
+    print("The BMI is: fuck you: " + str(bmi))
+
+calculate_bmi(weight=66, height=1.72)
