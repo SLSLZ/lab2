29c830915a8d237d9dfa8e18fc2868065cc3a8a6
SeanLim <seanlim.21@ichat.sp.edu.sg>
2022-11-01 09:47:57 +0800

    commit

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ def calculate_bmi(height, weight):
 
     bmi = (weight)/(height*height)
 
-    print("The BMI is: fuck you: " + str(bmi))
+    print("The BMI is: " + str(bmi))
 
 calculate_bmi(weight=66, height=1.72)
